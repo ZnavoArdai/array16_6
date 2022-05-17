dbb16ce70146deb29d38a88e0442a97bf8f8c1fc
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-05-17 20:33:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,136 @@
+//14
+// function ages(x=[]){
+// var y=[];
+//     for(var i=0;i<x.length;i++){
+//         if(x[i]>20){
+// y.push(x[i]);
+//         }
+//     }
+
+    
+// console.log(y);
+// }
+
+// ages([2,21,12,3,67,8,99,9,54,4,5,88,9,6,56]);
+
+//asaaf challenge
+// function checklist(num,x=[]){
+
+
+// while(num!=0){
+//     num=prompt("enter item list(to end list enter 0)");
+//     if(num!=0){
+//         x.push(num);
+//         num=confirm("do you want to end list?")
+
+//         if(num==true){
+//             num=0;
+//             num=prompt("enter item list(to end list enter 0)");
+
+//         }
+
+    
+       
+//     }
+// console.log(x);
+//     }
+// }
+
+
+// checklist();
+
+
+//15
+
+// function plusIndex(x=[]){
+
+
+//     for (var i=0;i<x.length;i++){
+// console.log(`${i}${x[i]}`)
+//         // x[i]=`${x[i]}` ;
+//         x[i]=`${i} ${x[i]}`;
+//     }
+
+//     console.log(x);
+// }
+
+// plusIndex(["avi","78","ron"]);
+ //16
+// function even(x=[]){
+// var y=[];
+//     for(var i=0;i<x.length;i++){
+//      if(x[i]%2==0){
+//          x[i]="even"
+//      }   
+//     }
+//     y=x;
+//     return console.log(y);
+// }
+
+// even([1,2,3,4,5,6,7,8]);
+
+//17
+
+// function numbers(num1,num2){
+// num1=parseInt(prompt("enter number 1"));
+// num2=parseInt(prompt("enter number 2"));
+
+// console.log(num1,num2);
+
+
+// }
+
+//18
+// function numbers(num1,num2){
+// num1=parseInt(prompt("enter number 1"));
+// num2=parseInt(prompt("enter number 2"));
+
+// var sum=num1+num2;
+// if(sum==13){
+//     console.log("bar mitzva");
+
+// }
+// else if(sum==12)
+// console.log("bat mitzva");
+// else
+// console.log("kid");
+
+
+
+// }
+
+//19
+
+// function person(age,fname,lname,num){
+//     var i=0;
+// if(age>18){
+// while( i<num){
+//     console.log(fname,lname)
+//     i++;
+// }
+// }
+
+// }
+
+// person(23,"sadsa","levi",46);
+//20
+
+function printJava(){
+for(var i=0;i<1000;i++){
+    console.log("javascript is not java")
+}
+}
+
+printJava();
+
+
+
+
+
+    
+
+
+   
+
+
+
